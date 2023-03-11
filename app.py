8a7ebaf50255ed9794384e1da1a7f605faa080e7
Ryan Whitemore <ryan.whitemore@gmail.com>
2023-03-10 20:55:19 -0500

    added delete functionality and download functionality

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
 from bill_object import Bill, User
-from flask import Flask, render_template, url_for, request, make_response
+from flask import Flask, render_template, url_for, request, make_response, redirect, Response
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 from flask_bcrypt import Bcrypt
 import mysql.connector as connector
-
+from fileinput import filename
 import os
 
+# load and define environment variables
 load_dotenv()
-
 user = os.environ['USER']
 password = os.environ['PASSWORD']
 db_name = os.environ['DB_NAME']
 
-
+# instatiate and configure flask app
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://{user}:{password}@localhost:3306/{db_name}".format(
-    user = user, password = password, db_name = db_name)
-db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
+
+# Define a function to make a connection to the mysql database
 def load_session():
     connection = connector.connect(user=user,
                                   password=password,
@@ -29,55 +28,74 @@ def load_session():
                                   auth_plugin='mysql_native_password')
     return connection
 
+
+# Login route
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     
     if request.method == "POST":
         
+        # Make connection to database for post actions
         session = load_session()
         cursor = session.cursor()
         
         if request.form['action'] == 'Register':
             username = request.form['username']
+            # Hash password before storing in database
             password = bcrypt.generate_password_hash(request.form['password'])
+            # define a tuple of the query variables to pass to cursor.exevute
             query_variables = (username, password)
             add_query = """
             INSERT INTO users (username, password)
             VALUES (%s, %s)"""
 
+            # Fetch all usernames currently in the database
             cursor.execute("""
             SELECT * FROM users""")
             results = cursor.fetchall()
+
+            # Check given username against existing
             for tuple in results:
                 if request.form['username'] in tuple:
                     return "username already exists"
+            # username doesnt already exist, add to database
             cursor.execute(add_query, query_variables)
             session.commit()
+            session.close()
             return 'registered'
         
         elif request.form['action'] == 'Login':
+            # Get username and password from user input
             username = (request.form['username'],)
-            print('username value is :', username)
             password_hash = str(request.form['password'])
+
+            # Fetch credentials from database to check against
             query = """SELECT password, user_id
             FROM users
             WHERE username = %s"""
 
             cursor.execute(query, username)
             results = cursor.fetchall()
+            session.close()
+            
             password = results[0][0]
             user_id = str(results[0][1])
-            print(user_id)
+            
+            # Check if password matches password in database
             if bcrypt.check_password_hash( str(password), password_hash):
+                
+                # Set response to index.html and set user_id cookie for future use
                 resp = make_response(render_template('index.html'))
                 resp.set_cookie('user_id', user_id)
                 return resp
             
             else:
+                session.close()
                 return 'invalid username or password'
     else:
         return render_template('login.html')
 
+# Route for adding billing info to database
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
@@ -87,6 +105,7 @@ def index():
 
         insert_query = """
         INSERT INTO bills (
+            customer_name,
             num_windows,
             num_entry_doors,
             num_french_doors,
@@ -97,9 +116,11 @@ def index():
             measure,
             user_id
         )
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
 
+        # initialize variables for query from the user input
         response = request.form
+        customer_name = request.form['customer']
         windows = request.form['windows']
         entry_doors = request.form['entry_doors']
         french_doors = request.form['french_doors']
@@ -108,13 +129,16 @@ def index():
         hours_labor = request.form['extra_labor']
         receipts = request.form['receipts']
         measure = request.form['measure']
+        # Convert string response for measure to make it TINYINT compatable
         if measure.upper() == 'YES':
             measure = 1
         else:
             measure = 0
         user_id = request.cookies.get('user_id')
         
-        values = (windows, 
+        # Set values tuple to pass to cursor.execute
+        values = (customer_name,
+                  windows, 
                   entry_doors, 
                   french_doors, 
                   sgd, 
@@ -126,10 +150,130 @@ def index():
         
         cursor.execute(insert_query, values)
         session.commit()
+        session.close()
 
-        return response
+        return redirect('/bills')
     else:
         return render_template('index.html')
+    
+    make_response
+
+@app.route('/bills', methods=['POST', 'GET'])
+def bills():
+
+    # load database connection
+    session = load_session()
+    cursor = session.cursor()
+    user_id = (request.cookies.get('user_id'),)
+          
+    # Query to select necessary information based on user_id
+    query = """
+    SELECT
+    bill_id,
+    num_windows,
+    num_entry_doors,
+    num_french_doors,
+    num_sgd,
+    num_sgd_extra_panels,
+    hours_extra_labor,
+    receipt_total,
+    measure,
+    customer_name
+    FROM bills
+    WHERE user_id = %s"""
+
+    cursor.execute(query, user_id)
+    
+    results = cursor.fetchall()
+
+    session.close()
+
+    return render_template('bills.html', results=results)
+
+
+# Route for deleting bills
+@app.route('/delete/<int:id>')
+def delete(id):
+
+    # Load database connection
+    session = load_session()
+    cursor = session.cursor()
+
+    # Query to find bill to delete
+    find_bill_query = """
+    SELECT bill_id
+    FROM bills
+    WHERE bill_id = %s"""
+
+    # Query to delete bill
+    delete_bill_query = """
+    DELETE FROM bills
+    WHERE bill_id = %s"""
+
+    # Turn bill id into tuple to pass to cursor.execute
+    bill_id = (id,)
+
+    # Execute  find_bill_query
+    cursor.execute(find_bill_query, bill_id)
+    # Fetch results from query
+    bill_to_delete = cursor.fetchall()
+
+    # Execute delete query
+    cursor.execute(delete_bill_query, bill_to_delete[0])
+    session.commit()
+    session.close()
+
+    return redirect('/bills')
+
+
+# App route to download bill pdf
+@app.route('/download/<int:id>')
+def download(id):
+    
+    # load session
+    session = load_session()
+    cursor = session.cursor()
+
+    # query to select necesarry info for bill
+    select_query = """
+    SELECT 
+    num_windows,
+    num_entry_doors,
+    num_french_doors,
+    num_sgd,
+    num_sgd_extra_panels,
+    hours_extra_labor,
+    receipt_total,
+    measure,
+    customer_name
+    FROM bills
+    WHERE bill_id = %s
+    """
+    
+    # make user_id tuple to pass to cursor.execute
+    user_id = (id,)
+
+    cursor.execute(select_query, user_id)
+    bill_to_download = cursor.fetchall()
+    bill_columns = cursor.column_names
+   
+    # instatiate bill calss
+    bill = Bill()
+    
+    # populate bill attributes with info from database
+    for i in range(len(bill_columns) - 1):
+        bill.update_attribute(bill_columns[i], bill_to_download[0][i])
+
+    # add customer name to bill
+    bill.add_name(bill_to_download[0][8])
+    
+    # Generate bill pdf
+    pdf = bill.generate_bill()
+
+
+    resp = Response(pdf.output(dest='S').encode('latin-1'), mimetype='application/pdf', headers={'Content-Disposition':'attatchment;filename={}'.format(bill.file_name)})
+
+    return resp
 
 if __name__ == "__main__":
     app.run(debug=True)
--- bill_object.py
+++ bill_object.py
@@ -2,19 +2,19 @@ from fpdf import FPDF
 
 class Bill:
     def __init__(self):
-        self.windows = 0
+        self.num_windows = 0
         self.windows_total = 0.00
-        self.entry_doors = 0
+        self.num_entry_doors = 0
         self.entry_doors_total = 0.00
-        self.french_doors = 0
+        self.num_french_doors = 0
         self.french_doors_total = 0.00
-        self.sgd = 0
+        self.num_sgd = 0
         self.sgd_total = 0.00
-        self.sgd_extra = 0
+        self.num_sgd_extra_panels = 0
         self.sgd_extra_total = 0.00
-        self.extra_labor = 0
+        self.hours_extra_labor = 0
         self.extra_labor_total = 0.00
-        self.receipts = 0
+        self.receipt_total = 0
         self.measure = 0
         self.total = 0
         self.customer_name = ''
@@ -22,7 +22,7 @@ class Bill:
 
     def update_attribute(self, attribute, input):
         if attribute == 'measure':
-            if str(input.upper()) == 'YES':
+            if input == 1:
                 self.measure = 125
             else:
                 pass
@@ -32,12 +32,12 @@ class Bill:
             setattr(self, attribute, input)
     
     def make_totals(self):
-        self.windows_total = int(self.windows) * 125
-        self.entry_doors_total = int(self.entry_doors) * 300
-        self.french_doors_total = int(self.french_doors) * 400
-        self.sgd_total = int(self.sgd) * 300
-        self.sgd_extra_total = int(self.sgd_extra) * 50
-        self.extra_labor_total = int(self.extra_labor) * 80 
+        self.windows_total = int(self.num_windows) * 125
+        self.entry_doors_total = int(self.num_entry_doors) * 300
+        self.french_doors_total = int(self.num_french_doors) * 400
+        self.sgd_total = int(self.num_sgd) * 300
+        self.sgd_extra_total = int(self.num_sgd_extra_panels) * 50
+        self.extra_labor_total = int(self.hours_extra_labor) * 80 
     
     def add_receipts(self, input):
         self.receipts = float(input)
@@ -54,7 +54,7 @@ class Bill:
 
     def get_bill_total(self):
         self.make_totals()
-        self.total = float(self.receipts) + \
+        self.total = float(self.receipt_total) + \
             int(self.windows_total) + \
             int(self.entry_doors_total) + \
             int(self.french_doors_total) + \
@@ -76,6 +76,7 @@ class Bill:
 
     
     def generate_bill(self):
+        self.get_bill_total()
         pdf = FPDF()
         pdf.add_page()
         pdf.set_font('Arial', 'B', 16)
@@ -86,26 +87,26 @@ class Bill:
         # create first row
         self.create_row('Type', 'Number', 'Total', pdf, top, offset, 10)
         # create second row
-        self.create_row('windows', self.windows, self.windows_total, pdf, top, offset, 20)
+        self.create_row('windows', self.num_windows, self.windows_total, pdf, top, offset, 20)
         # create third row
-        self.create_row('Entry doors', self.entry_doors, self.entry_doors_total, pdf, top, offset, 30)
+        self.create_row('Entry doors', self.num_entry_doors, self.entry_doors_total, pdf, top, offset, 30)
         # create fourth row
-        self.create_row('French doors', self.french_doors, self.french_doors_total, pdf, top, offset, 40)
+        self.create_row('French doors', self.num_french_doors, self.french_doors_total, pdf, top, offset, 40)
         # create fith row
-        self.create_row('Sliding doors', self.sgd, self.sgd_total, pdf, top, offset, 50)
+        self.create_row('Sliding doors', self.num_sgd, self.sgd_total, pdf, top, offset, 50)
         # create sixth row
-        self.create_row('Extra panels', self.sgd_extra, self.sgd_extra_total, pdf, top, offset, 60)
+        self.create_row('Extra panels', self.num_sgd_extra_panels, self.sgd_extra_total, pdf, top, offset, 60)
         # create seventh row
-        self.create_row('Hours labor', self.extra_labor, self.extra_labor_total, pdf, top, offset, 70)
+        self.create_row('Hours labor', self.hours_extra_labor, self.extra_labor_total, pdf, top, offset, 70)
         # create eighth row
-        self.create_row('Receipts', ' ', self.receipts, pdf, top, offset, 80)
+        self.create_row('Receipts', ' ', self.receipt_total, pdf, top, offset, 80)
         # create ninth row
         self.create_row('Measure', ' ', self.measure, pdf, top, offset, 90)
         # create total row
         pdf.x = offset + 40
         pdf.multi_cell(40, 10, str(self.total), 1, 0)
 
-        pdf.output(self.file_name)
+        return pdf
 
 class User:
     def __init__(self, user, password):
